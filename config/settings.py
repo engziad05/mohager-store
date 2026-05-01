19a7e260054a79f60fdfdc64a4caf2713776e6c9
Ziad <ziad0453@gmail.com>
2026-05-01 18:29:29 +0300

    fix: simplify static settings and fix media root

--- config/settings.py
+++ config/settings.py
@@ -123,12 +123,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 # Custom user model
 AUTH_USER_MODEL = 'accounts.CustomUser'
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
