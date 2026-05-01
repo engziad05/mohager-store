01d256f1788673f6778891273c1dcf3af79524e1
Ziad <ziad0453@gmail.com>
2026-05-01 18:25:20 +0300

    fix: add missing slash to STATIC_URL

--- config/settings.py
+++ config/settings.py
@@ -118,12 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATICFILES_DIRS = [BASE_DIR / 'static']
 # Custom user model
 AUTH_USER_MODEL = 'accounts.CustomUser'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Media files
 MEDIA_URL = '/media/'
