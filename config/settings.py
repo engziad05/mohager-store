d732a461508f94ea3ddc855e25b342b6f0987331
Ziad <ziad0453@gmail.com>
2026-05-01 18:07:39 +0300

    fix: add railway domain to CSRF_TRUSTED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-e59a2.up.railway.app']
 
 # Application definition
 
