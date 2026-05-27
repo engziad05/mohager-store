18e7fcb1bac7046c698a8004cf9fba0d19bf6409
Ziad <ziad0453@gmail.com>
2026-05-27 19:38:06 +0300

    Fix password change form in unfold admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,18 @@
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from unfold.admin import ModelAdmin
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 
 from .models import CustomUser
 
 
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(BaseUserAdmin, ModelAdmin):
     model = CustomUser
+    
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
 
-    fieldsets = UserAdmin.fieldsets + (
+    fieldsets = BaseUserAdmin.fieldsets + (
         ('Mohager profile', {'fields': ('phone', 'preferred_lang')}),
     )
     list_display = ['username', 'email', 'phone', 'preferred_lang', 'is_staff', 'is_active']
